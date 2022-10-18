c01404b0800cc116ace214ccec83b0de38ba7346
Christopher Ruz <christopher.ruz@bci.cl>
2022-10-18 18:04:24 -0300

    Add lifecycle extension

--- feature/characterslist/src/main/kotlin/cl/gringraz/marvelcatalog/feature/characterslist/presentation/Extensions.kt
+++ feature/characterslist/src/main/kotlin/cl/gringraz/marvelcatalog/feature/characterslist/presentation/Extensions.kt
@@ -1,4 +1,16 @@
 package cl.gringraz.marvelcatalog.feature.characterslist.presentation
 
-class Extensions {
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+fun LifecycleOwner.safeLifecycle(block: suspend CoroutineScope.() -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            block()
+        }
+    }
 }
